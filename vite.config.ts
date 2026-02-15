c3285783d0c702f9b70b8922beab617e4aae65c2
thewordiwishedtosay <15915398518@tutanota.com>
2026-02-15 01:39:48 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Backroom-web/', // <--- Add this line!
+  base: '/backroom-web/', // <--- Add this line!
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
